2f272398faaaf456f2bb6f84afcdb1ab20b9972a
NiZeR <nimishm27@gmail.com>
2021-03-20 21:03:42 +0530

    removing secret key

--- Fire_auth/settings.py
+++ Fire_auth/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ps%g_!%)2hbeh+_v%hz((4a@=4#+foez$&o+9d+e-h!u2n4ggf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
